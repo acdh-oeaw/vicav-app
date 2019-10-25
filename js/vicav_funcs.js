30f475547ee8cf980328ff84f1546c28fb050d7a
Omar Siam <Omar.Siam@oeaw.ac.at>
2019-10-25 12:26:11 +0200

    Add cookie consent and prepare for using matomo. Initialization for acdh matomo is a customization on the server.

--- js/vicav_funcs.js
+++ js/vicav_funcs.js
@@ -1867,3 +1867,42 @@ function copyToClipboard(text) {
         }
     }
 }
+
+/** COOKIES **/
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+//Cookies warning first page
+var cookiesAccepted = getCookie("cookiesAccepted");
+if (!cookiesAccepted) {
+    $("#cookie-overlay").fadeIn(100);
+}
+
+//Accept cookies
+$(".cookie-accept-btn").on('click', function(){
+    setCookie("cookiesAccepted", true, 180);
+    $("#cookie-overlay").fadeOut(100);
+});
+
+/** COOKIES END **/
+
