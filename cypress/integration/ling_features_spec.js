613f2d9670e6f1e51438405bc63e362a55ed7cd0
Zsofia Abraham <zsofia.abraham@oeaw.ac.at>
2020-01-22 13:54:58 +0100

    Working features comparison test

--- cypress/integration/ling_features_spec.js
+++ cypress/integration/ling_features_spec.js
@@ -20,21 +20,27 @@ describe('Features XSLT', function() {
 })
 
 describe('Features Comparison', function() {
-    it('should output the table', function() {
+    it('should output the comparison table', function() {
 		cy.visit('http://localhost:8984/vicav/#map=[biblMarkers,_features_,]')
-		cy.contains('Feature Lists').click().then(() => {
-			cy.contains('Cross-examine the VICAV Feature Lists').click().then(() => {
-				cy.contains('who?').click().then(() => {
-					cy.contains('hāḏa r-rayyāl yāhu? / yāhu hāḏa r-rayyāl? / zəlma– əhwa ṣāḥbi. (Who is this man? – He is my friend)')
+		cy.get('button.navbar-toggler').click().then(() => {
+			cy.contains('Feature Lists').click().then(() => {
+				cy.contains('Cross-examine the VICAV Feature Lists').scrollIntoView().click().then(() => {
+					cy.contains('who?').click().then(() => {
+						cy.contains('Ahwaz')
+						cy.contains('Baghdad')
+						cy.contains('hāḏa r-rayyāl yāhu? / yāhu hāḏa r-rayyāl? / zəlma– əhwa ṣāḥbi.')
+						cy.contains('(Who is this man? – He is my friend)')								
+					});
 				});
 			});
-		});
 
-    	cy.fixture('api/lingfeatures_Test_body.xml').then(tunisBody => {
-		    cy.request('http://localhost:8984/vicav/profile?coll=vicav_lingfeatures&id=ling_features_test&xslt=features_01.xslt').as('ling_features_test')
-		    cy.get('@ling_features_test').then(response => {
-		    	expect(response.body.replace(/\s*(\r\n|\n|\r)\s*/gm, '')).to.eq(tunisBody.replace(/\s*(\r\n|\n|\r)\s*/gm, ''))
-		    })
-    	})
+	    	cy.fixture('api/lingfeatures_Test_body.xml').then(tunisBody => {
+			    cy.request('http://localhost:8984/vicav/profile?coll=vicav_lingfeatures&id=ling_features_test&xslt=features_01.xslt').as('ling_features_test')
+			    cy.get('@ling_features_test').then(response => {
+			    	expect(response.body.replace(/\s*(\r\n|\n|\r)\s*/gm, '')).to.eq(tunisBody.replace(/\s*(\r\n|\n|\r)\s*/gm, ''))
+			    })
+	    	})
+
+		})
     });
 })
