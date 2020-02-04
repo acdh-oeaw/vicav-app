86d0225be0bddfc00c381b31448df44a15f8c3d7
Zsofia Abraham <zsofia.abraham@oeaw.ac.at>
2020-02-04 12:17:18 +0100

    Add test for homepage with and without config

---
+++ cypress/integration/homepage_config_spec.js
@@ -0,0 +1,22 @@
+describe('Home page', function() {
+    it('should show whole Middle-East by deafult', function() {
+		cy.task('deleteFile', 'config.json').then(() => {
+	    cy.visit('http://localhost:8984/vicav/')
+	    cy.get('img.leaflet-marker-icon') // Wait until the initial markers appear.
+
+	    cy.get('img[alt^="Khorasan"]').should('be.visible');
+
+		});
+    })
+
+    it('should show Tunisia with different config', function() {
+    	cy.fixture('tunocent.config.json').then((fixture) => {
+    	cy.writeFile('config.json', fixture);
+
+	    cy.visit('http://localhost:8984/vicav/')
+	    cy.get('img.leaflet-marker-icon') // Wait until the initial markers appear.
+
+	    cy.get('img[alt^="Khorasan"]').should('not.be.visible');
+    	});
+    })
+})
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -11,7 +11,19 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
+const fs = require('fs')
+
 module.exports = (on, config) => {
+
+  on('task', {
+    deleteFile (filename) {
+      if (fs.unlinkSync(filename)) {
+        return true
+      }
+
+      return null
+    }
+  });
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
 }
