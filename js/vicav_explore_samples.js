712200c76110dd9c590e656461f11fb5b02f825b
Zsofia Abraham <zsofia.abraham@oeaw.ac.at>
2020-03-30 12:19:37 +0200

    Cleanup code

--- js/vicav_explore_samples.js
+++ js/vicav_explore_samples.js
@@ -14,9 +14,9 @@ oms.addListener('click', function(marker) {
 });
 
 function createDisplayCrossSamplesPanel(query_, pID_ = '', pVisiblity_ = 'open', pURL_ = false) {
-    var pID = appendPanel('', "crossSamplesForm", "", "grid-wrap", '', 'hasTeiLink', '', 'compare-samples', pID_, pVisiblity_, pURL_);
-    var $root = $('[data-pid=' + pID + ']');
-    $( 'div[data-pid=' + pID + '] .grid-wrap' ).load( "compare-samples.html form" , function() {
+    $( '<div>' ).load( "compare-samples.html form" , function(event) {
+        var pID = appendPanel(this.innerHTML, "crossSamplesForm", "", "grid-wrap", '', 'hasTeiLink', '', 'compare-samples', pID_, pVisiblity_, pURL_);
+        var $root = $('[data-pid=' + pID + ']');
         $('form.compare-samples', $root).submit(function(event) {
             event.preventDefault();
             formSubmit($root, function (result, query) {
@@ -42,53 +42,11 @@ function createCrossSamplesResultsPanel(contents_ = '', query_ = '', pID_ = '', 
     }
 }
 
-function showExploreSamples(query_, sentences_, type_) {
-    if (type_ == 'tei') {
-        qs = './explore_samples?query=' + encodeURIComponent(query_) + '&sentences=' + encodeURIComponent(sentences_) + '&xslt=tei_2_html__v004__gen.xslt';
-    } else {
-        qs = './explore_samples?query=' + encodeURIComponent(query_) + '&sentences=' + encodeURIComponent(sentences_) + '&xslt=cross_samples_01.xslt';
-    }
-
-    $.ajax({
-        url: qs,
-        type: 'GET',
-        dataType: 'html',
-        cache: false,
-        crossDomain: true,
-        contentType: 'application/html; ',
-        success: function (result) {
-            if (result.includes('error type="user authentication"')) {
-                alert('Error: authentication did not work');
-            } else {
-                //appendPanel(result, "featureQuery", "features", "grid-wrap", '', 'hasTeiLink', '', snippetID_, pID_, pVisiblity_, pURL_);
-                //console.log(result);
-                // result = result.replace(/==teiFuncID==/, '#' + ana_);
-                // result = result.replace(/==teiFuncLabel==/, expl_);
-                
-                ob = document.getElementById("dvCrossSamples");
-                if (ob) {
-                  ob.innerHTML = result;
-                } else {
-                  createDisplayCrossSamplesPanel();  
-                  ob = document.getElementById("dvCrossSamples");
-                  if (ob) {
-                    ob.innerHTML = result;
-                  }
-                }
-            }
-        },
-        error: function (jqXHR, textStatus, errorThrown) {
-            alert(errorThrown);
-        }
-    });
-}
-
 // Navigation entry
 $("#liExploreSamples").mousedown (function (event) {
     clearMarkerLayers();
     insertSampleMarkers();
     adjustNav(this.id, "#subNavSamplesGeoRegMarkers");
-	createDisplayCrossSamplesPanel();
-//    showExploreSamples('tunis_sample_01,garia_sample_01', '1', 'html');
+	createDisplayCrossSamplesPanel('','', 'open', false);
 });
 
