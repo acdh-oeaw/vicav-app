be9e38422b65082a662c16cafba4a2f43c121e6d
Zsofia Abraham <zsofia.abraham@oeaw.ac.at>
2020-05-19 14:41:10 +0200

    Lets see this

--- scripts/run-xspec.sh
+++ scripts/run-xspec.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-success=$(${CI_BUILDS_DIR}/../bin/xspec ${1} 2>&1)
+success=$(${CI_BUILDS_DIR}/xspec/bin/xspec ${1} 2>&1)
 
 if [[ "$(echo ${success} | grep 'failure: 0')" == "" ]]; then
 	echo ${success} >> errors
--- scripts/run-xspecs.sh
+++ scripts/run-xspecs.sh
@@ -5,7 +5,7 @@ find ./xspec -name "*.xspec" \( -exec ./scripts/run-xspec.sh {} \; -o -quit \)
 
 if [ -f errors ]; then 
 	rm errors
-	1>&2 echo 'Some tests failed. See artifacts/XUNIT_OUT.txt'
+	echo 'Some tests failed. See artifacts/xspec'
 	exit 1
 fi
 
