ec5370d403283252c0b465b7c2dbb6d4db2656ba
Zsofia Abraham <zsofia.abraham@oeaw.ac.at>
2020-05-20 15:12:18 +0200

    Test script

--- scripts/run-xspec.sh
+++ scripts/run-xspec.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -euo pipefail
 
+${CI_BUILDS_DIR}/../xspec/bin/xspec.sh ${1} 2>&1
+
 success=$(${CI_BUILDS_DIR}/../xspec/bin/xspec.sh ${1} 2>&1)
 
 if [[ "$(echo ${success} | grep 'failed: 0')" == "" ]]; then
