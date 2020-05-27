a20478ff227ccc6e5abea714516151a0e20f1a98
Zsofia Abraham <zsofia.abraham@oeaw.ac.at>
2020-05-27 09:49:31 +0200

    Better script for xspec results

--- scripts/run-xspecs.sh
+++ scripts/run-xspecs.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 find ./xspec -name "*.xspec" \( -exec ./scripts/run-xspec.sh {} \; -o -quit \)
 
 if [ -f errors ]; then 
-	cp errors $GIT_CLONE_PATH/artifacts/xspec/XPEC_ERRORS
+	cp errors $GIT_CLONE_PATH/artifacts/xspec/XPEC_ERRORS.txt
 	rm errors
 	1>&2 echo 'Some tests failed. See artifacts/xspec'
 	exit 1
