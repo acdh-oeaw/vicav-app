9da6d513a5bfb02c940b6d6f365f871a9f33954e
Zsofia Abraham <zsofia.abraham@oeaw.ac.at>
2020-06-19 14:47:48 +0200

    Proper doc update

--- cypress/integration/homepage_config_spec.js
+++ cypress/integration/homepage_config_spec.js
@@ -2,7 +2,7 @@ describe('Home page', function() {
     it('should show Tunisia with different config', function() {
     	cy.fixture('../../fixtures/vicav_projects/map.xml').then((fixture) => {
     	cy.writeFile('vicav_projects/vicav.xml', fixture);
-//    	cy.exec(Cypress.env('BASEX_ROOT') + '/bin/basexclient -Uadmin -Padmin -c "DROP DATABASE vicav_projects;CREATE DATABASE vicav_projects $(pwd)/vicav_projects"')
+    	cy.exec(Cypress.env('BASEX_ROOT') + '/bin/basexclient -Uadmin -Padmin -c "OPEN vicav_projects; REPLACE vicav.xml $(pwd)/fixtures/vicav_projects/map.xml"')
 
 	    cy.visit('http://localhost:8984/vicav/')
 	    cy.get('img.leaflet-marker-icon') // Wait until the initial markers appear.
@@ -16,7 +16,7 @@ describe('Home page', function() {
 	    cy.get('img.leaflet-marker-icon') // Wait until the initial markers appear.
     	cy.fixture('../../fixtures/vicav_projects/vicav.xml').then((fixture) => {
 	    	cy.writeFile('vicav_projects/vicav.xml', fixture);
-//	    	cy.exec(Cypress.env('BASEX_ROOT') + '/bin/basexclient -Uadmin -Padmin -c "DROP DATABASE vicav_projects;CREATE DATABASE vicav_projects $(pwd)/vicav_projects"')
+	    	cy.exec(Cypress.env('BASEX_ROOT') + '/bin/basexclient -Uadmin -Padmin -c "OPEN vicav_projects; REPLACE vicav.xml $(pwd)/fixtures/vicav_projects/vicav.xml"')
 		   	cy.visit('http://localhost:8984/vicav/')
 	    	cy.get('img.leaflet-marker-icon') // Wait until the initial markers appear.
 			cy.get('img[alt^="Khorasan"]').should('be.visible');
