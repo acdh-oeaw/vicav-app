481e4eff658ec934035075622d1e40c86cd074c1
Zsofia Abraham <zsofia.abraham@oeaw.ac.at>
2020-06-19 10:44:52 +0200

    Fix test

--- cypress/integration/homepage_config_spec.js
+++ cypress/integration/homepage_config_spec.js
@@ -1,10 +1,4 @@
 describe('Home page', function() {
-    it('should show whole Middle-East by deafult', function() {
-	    cy.visit('http://localhost:8984/vicav/')
-	    cy.get('img.leaflet-marker-icon') // Wait until the initial markers appear.
-	    cy.get('img[alt^="Khorasan"]').should('be.visible');
-    })
-
     it('should show Tunisia with different config', function() {
     	cy.fixture('../../fixtures/vicav_projects/map.xml').then((fixture) => {
     	cy.writeFile('vicav_projects/vicav.xml', fixture);
@@ -14,8 +8,16 @@ describe('Home page', function() {
 
 	    cy.get('img[alt^="Khorasan"]').should('not.be.visible');
     	});
+    })
+
+	it('should show whole Middle-East by deafult', function() {
+	    cy.visit('http://localhost:8984/vicav/')
+	    cy.get('img.leaflet-marker-icon') // Wait until the initial markers appear.
     	cy.fixture('../../fixtures/vicav_projects/vicav.xml').then((fixture) => {
 	    	cy.writeFile('vicav_projects/vicav.xml', fixture);
+		   	cy.visit('http://localhost:8984/vicav/')
+	    	cy.get('img.leaflet-marker-icon') // Wait until the initial markers appear.
+			cy.get('img[alt^="Khorasan"]').should('be.visible');
 	    }
     })
 })
