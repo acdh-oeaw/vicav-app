810f0302f11ff91661fcc2aa566f65245e5bbd63
Omar Siam <Omar.Siam@oeaw.ac.at>
2022-06-20 15:11:27 +0200

    Need to fetch build file, was removed from distribution

--- postinstall.cmd
+++ postinstall.cmd
@@ -6,6 +6,7 @@ for %%f in (node_modules\jquery\dist\jquery.js node_modules\jquery\dist\jquery.m
 cd node_modules\jquery-ui
 del /s /q dist
 cmd /c "npm install"
+curl -lO https://raw.githubusercontent.com/jquery/jquery-ui/main/Gruntfile.js
 cmd /c "node_modules\.bin\grunt concat:css requirejs uglify"
 cd ..\..
 for %%f in (node_modules\jquery-ui\dist\jquery-ui.js node_modules\jquery-ui\dist\jquery-ui.min.js) do copy %%f js
@@ -13,4 +14,3 @@ copy node_modules\leaflet\dist\leaflet.css vendor
 for %%f in (node_modules\leaflet\dist\leaflet.js node_modules\leaflet\dist\leaflet.js.map) do copy %%f js
 for %%f in (node_modules\popper.js\dist\umd\popper.js node_modules\popper.js\dist\umd\popper.js.map) do copy %%f js
 for %%f in (node_modules\popper.js\dist\umd\popper.min.js node_modules\popper.js\dist\umd\popper.min.js.map) do copy %%f js
-
