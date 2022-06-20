1cd03b9b149defe02e1faddbea3d06229a3ef466
Omar Siam <Omar.Siam@oeaw.ac.at>
2022-06-20 15:01:59 +0200

    Update dependencies

--- deployment/initial.sh
+++ deployment/initial.sh
@@ -7,7 +7,8 @@ mv redeploy.settings.dist redeploy.settings
 git clone https://github.com/acdh-oeaw/vicav-content
 if [ "${STACK}x" = "x" ]; then
 pushd lib/custom
-curl -LO https://repo1.maven.org/maven2/net/sf/saxon/Saxon-HE/10.3/Saxon-HE-10.3.jar
+curl -LO https://repo1.maven.org/maven2/net/sf/saxon/Saxon-HE/11.3/Saxon-HE-11.3.jar
+curl -LO https://repo1.maven.org/maven2/org/xmlresolver/xmlresolver/4.3.0/xmlresolver-4.3.0.jar
 popd
 if [ "$OSTYPE" == "msys" -o "$OSTYPE" == "win32" ]
 then
