014757e5800585014b5ac482463a930023058355
Omar Siam <Omar.Siam@oeaw.ac.at>
2022-06-23 13:47:42 +0200

    pwd isn't portable when used like this

--- cypress/integration/homepage_config_spec.js
+++ cypress/integration/homepage_config_spec.js
@@ -2,14 +2,14 @@ describe('Home page', function() {
     const BASEX_ROOT = Cypress.env('BASEX_ROOT') || '/app/.heroku/basex'
     const BASEX_PWD = Cypress.env('BASEX_admin_pw') || 'admin'
     it('should show Tunisia with different config', function() {
-    	cy.exec(`${BASEX_ROOT}/bin/basexclient -Uadmin -P${BASEX_PWD} -c "OPEN vicav_projects; REPLACE vicav.xml $(pwd)/fixtures/vicav_projects/map.xml"`)
+    	cy.exec(`${BASEX_ROOT}/bin/basexclient -Uadmin -P${BASEX_PWD} -c "OPEN vicav_projects; REPLACE vicav.xml ${Cypress.config()['fileServerFolder']}/fixtures/vicav_projects/map.xml"`)
 	    cy.visit('http://localhost:8984/vicav/')
 	    cy.get('img.leaflet-marker-icon') // Wait until the initial markers appear.
 	    cy.get('img[alt^="Khorasan"]').should('not.be.visible');
     })
 
 	it('should show whole Middle-East by deafult', function() {
-    	cy.exec(`${BASEX_ROOT}/bin/basexclient -Uadmin -P${BASEX_PWD} -c "OPEN vicav_projects; REPLACE vicav.xml $(pwd)/fixtures/vicav_projects/vicav.xml"`)
+    	cy.exec(`${BASEX_ROOT}/bin/basexclient -Uadmin -P${BASEX_PWD} -c "OPEN vicav_projects; REPLACE vicav.xml ${Cypress.config()['fileServerFolder']}/fixtures/vicav_projects/vicav.xml"`)
 	   	cy.visit('http://localhost:8984/vicav/')
     	cy.get('img.leaflet-marker-icon') // Wait until the initial markers appear.
 		cy.get('img[alt^="Khorasan"]').should('be.visible');
