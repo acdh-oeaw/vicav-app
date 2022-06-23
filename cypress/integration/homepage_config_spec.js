14096ebb5fccc88da676c7ccceaf7a78eafbe284
Omar Siam <Omar.Siam@oeaw.ac.at>
2022-06-23 15:40:00 +0200

    Add cookies accept handling

--- cypress/integration/homepage_config_spec.js
+++ cypress/integration/homepage_config_spec.js
@@ -4,6 +4,9 @@ describe('Home page', function() {
     it('should show Tunisia with different config', function() {
     	cy.exec(`${BASEX_ROOT}/bin/basexclient -Uadmin -P${BASEX_PWD} -c "OPEN vicav_projects; REPLACE vicav.xml ${Cypress.config()['fileServerFolder']}/fixtures/vicav_projects/map.xml"`)
 	    cy.visit('http://localhost:8984/vicav/')
+		cy.get('#cookie-overlay').should('be.visible')
+		cy.get('.cookie-accept-btn').click()
+		cy.get('#cookie-overlay').should('not.be.visible')
 	    cy.get('img.leaflet-marker-icon') // Wait until the initial markers appear.
 	    cy.get('img[alt^="Khorasan"]').should('not.be.visible');
     })
@@ -11,6 +14,9 @@ describe('Home page', function() {
 	it('should show whole Middle-East by deafult', function() {
     	cy.exec(`${BASEX_ROOT}/bin/basexclient -Uadmin -P${BASEX_PWD} -c "OPEN vicav_projects; REPLACE vicav.xml ${Cypress.config()['fileServerFolder']}/fixtures/vicav_projects/vicav.xml"`)
 	   	cy.visit('http://localhost:8984/vicav/')
+		cy.get('#cookie-overlay').should('be.visible')
+		cy.get('.cookie-accept-btn').click()
+		cy.get('#cookie-overlay').should('not.be.visible')
     	cy.get('img.leaflet-marker-icon') // Wait until the initial markers appear.
 		cy.get('img[alt^="Khorasan"]').should('be.visible');
     })
