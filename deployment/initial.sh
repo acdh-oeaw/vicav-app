e7f50473f4b20f092d13b369548e3608fe343fa1
Omar Siam <simar@gmx.net>
2023-03-03 11:52:02 +0100

    redeploy.settings  is now part of content Patch it on devel branch

--- deployment/initial.sh
+++ deployment/initial.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 CONTENT_REPO=${CONTENT_REPO:-https://github.com/acdh-oeaw/vicav-content}
+branch_name=$(git rev-parse --abbrev-ref HEAD)
 cp -Rv ./deployment/* ${1:-../../}
 npm install
 cd ${1:-../../}
 git clone $CONTENT_REPO
 cp -Rv ./*-content/deployment/* .
 mv redeploy.settings.dist redeploy.settings
+if [ $branch_name == "devel" ]
+then sed 's/onlytags=true.*/onlytags=false # enable for production/g' -i redeploy.settings
+fi
 if [ "${STACK}x" = "x" ]; then
 pushd lib/custom
 curl -LO https://repo1.maven.org/maven2/net/sf/saxon/Saxon-HE/11.4/Saxon-HE-11.4.jar
