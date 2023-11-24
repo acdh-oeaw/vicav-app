8fd66d470248853763494877711c5dba378a0c78
Omar Siam <simar@gmx.net>
2023-11-24 23:32:56 +0100

    Update to Cypress 13
    Need to explicitly enable video.

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,8 @@ module.exports = defineConfig({
   defaultCommandTimeout: 20000,
   viewportHeight: 768,
   viewportWidth: 1440,
+  video: true,
+  videoCompression: true,
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
